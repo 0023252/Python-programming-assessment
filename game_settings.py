ea53b042583f3db4c6684725bfcb1451b92ab622
John <mj281007@gmail.com>
2025-06-05 10:01:05 +1200

    losHznx

--- game_settings.py
+++ game_settings.py
@@ -3,3 +3,4 @@ import pygame
 player_sprite = [pygame.image.load("Colored_character.png"),
                  pygame.image.load("Colored_character(1).png"),
                  pygame.image.load("Colored_character(2).png")]
+
