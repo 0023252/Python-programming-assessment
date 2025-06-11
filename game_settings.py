90c08ad3cece451134c9acf3aa58db38d55a4026
John <mj281007@gmail.com>
2025-06-12 09:43:48 +1200

    dzv dfgzdf

--- game_settings.py
+++ game_settings.py
@@ -5,6 +5,8 @@ player_sprite = [pygame.image.load("Colored_character.png"),
                  pygame.image.load("Colored_character(1).png"),
                  pygame.image.load("Colored_character(2).png"),
                  pygame.image.load("Colored_character(3).png"),
+                 pygame.image.load("Colored_character(4).png"),
+                 pygame.image.load("Colored_character(3).png"),
                  pygame.image.load("Colored_character(2).png"),
-                 pygame.image.load("Colored_character(4).png")]
+                 pygame.image.load("Colored_character(1).png")]
 
